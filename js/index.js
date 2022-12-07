0854fc6e801bf30e5f8384c2b3cd279282e1db93
Zefanja Timorason <z.timorason@gmail.com>
2022-12-07 22:57:42 +0100

    some changes

--- js/index.js
+++ js/index.js
@@ -3,18 +3,24 @@ class Game {
     this.ctx = undefined;
     this.bg = undefined;
     this.player = undefined;
+    this.obstacle = undefined;
+    this.frame = 0;
+    this.myObstacles = [];
   }
   startGame() {
     const canvas = document.getElementById("canvas");
     this.ctx = canvas.getContext("2d");
     const car = new Car(100, 100, 250, 400);
+    const block1 = new Block()
     this.player = car;
+    this.obstacle = block1;
     const background = new Image();
     background.src = "/images/road.png";
     background.onload = () => {
       this.bg = background;
       this.updateCanvas();
       this.drawPlayer();
+      this.drawObstacle();
     };
   }
 
@@ -27,12 +33,34 @@ class Game {
       this.player.height
     );
   }
-  drawObstacle() {}
+
+  drawObstacle() {
+    this.myObstacles.forEach((block) => {
+      this.ctx.drawImage(
+        this.obstacle.block,
+        block.posX,
+        block.posY,
+        block.width,
+        block.height
+      );
+      if (block.posY > 501) {
+        this.myObstacles.shift();
+      }
+    });
+  }
+
   updateCanvas() {
     setInterval(() => {
       this.ctx.clearRect(0, 0, 500, 700);
       this.ctx.drawImage(this.bg, 0, 0, 500, 700);
       this.drawPlayer();
+
+      this.frame += 5;
+      if (this.frame % 120 === 0) {
+        let blockk = Math.random() * 410;
+        const newBlock = new Block(40, 40, blockk, 0);
+        this.myObstacles.push(newBlock);
+      }
     }, 20);
   }
 }
@@ -67,6 +95,24 @@ class Car {
         this.moveLeft();
         break;
     }
+  }
+}
+
+class Block {
+  constructor(height, width, posX, posY) {
+    this.height = height;
+    this.width = width;
+    this.posX = posX;
+    this.posY = posY;
+
+    this.block = this.createBlock();
+  }
+
+  imageBlock() {
+    
+  }
+  createBlock() {
+    
   }
 }
 window.onload = () => {
