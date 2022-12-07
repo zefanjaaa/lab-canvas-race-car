aad3e6c32242c78bbec3d49c71de9e4a3f0bdb86
Zefanja Timorason <z.timorason@gmail.com>
2022-12-07 21:16:06 +0100

    first start

--- js/index.js
+++ js/index.js
@@ -1,7 +1,27 @@
+class Game {
+  constructor() {
+    this.ctx = undefined;
+    this.bg = undefined;
+    this.player = undefined;
+  }
+  startGame() {
+    const canvas = document.getElementById("canvas");
+    this.ctx = canvas.getContext("2d");
+    const car = new Car(45, 100, 250, 400);
+    this.player = car;
+    const background = new Image();
+    background.src = "/images/road.png";
+    background.onload = () => {
+      this.bg = background;
+      this.updateCanvas();
+      this.drawPlayer();
+    };
+  }
+  
+}
 window.onload = () => {
-  document.getElementById('start-button').onclick = () => {
-    startGame();
+  document.getElementById("start-button").onclick = () => {
+    const gamez = new Game();
+    gamez.startGame();
   };
-
-  function startGame() {}
 };
