908087fc93b8dfbce94740575767c1abbe5a62f3
Zefanja Timorason <z.timorason@gmail.com>
2022-12-07 21:43:05 +0100

    added more

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ class Game {
   startGame() {
     const canvas = document.getElementById("canvas");
     this.ctx = canvas.getContext("2d");
-    const car = new Car(45, 100, 250, 400);
+    const car = new Car(100, 100, 250, 400);
     this.player = car;
     const background = new Image();
     background.src = "/images/road.png";
@@ -17,11 +17,64 @@ class Game {
       this.drawPlayer();
     };
   }
-  
+
+  drawPlayer() {
+    this.ctx.drawImage(
+      this.player.img,
+      this.player.posX - this.player.width / 2,
+      this.player.posY,
+      this.player.width,
+      this.player.height
+    );
+  }
+  drawObstacle() {}
+  updateCanvas() {
+    setInterval(() => {
+      this.ctx.clearRect(0, 0, 500, 700);
+      this.ctx.drawImage(this.bg, 0, 0, 500, 700);
+      this.drawPlayer();
+    }, 20);
+  }
+}
+
+class Car {
+  constructor(height, width, posX, posY) {
+    this.width = width;
+    this.height = height;
+    this.posX = posX;
+    this.posY = posY;
+    this.img = this.createCar();
+  }
+  createCar() {
+    const car = new Image();
+    car.src = "/images/car.png";
+    return car;
+  }
+
+  moveRight() {
+    this.posX += 12;
+  }
+  moveLeft() {
+    this.posX -= 12;
+  }
+  move(event) {
+    switch (event) {
+      case "ArrowRight":
+        this.moveRight();
+        break;
+
+      case "ArrowLeft":
+        this.moveLeft();
+        break;
+    }
+  }
 }
 window.onload = () => {
   document.getElementById("start-button").onclick = () => {
     const gamez = new Game();
     gamez.startGame();
+    document.addEventListener("keydown", (e) => {
+      gamez.player.move(e.key);
+    });
   };
 };
